bc14140626527a68fb39ddd8eeef86ec6149f357
Greg Nokes <tsykoduk@gmail.com>
2016-11-17 09:14:52 -0800

    make it purple

--- index.php
+++ index.php
@@ -1,14 +1,10 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//EN">
-<style media="screen" type="text/css">
-	background-color: purple;
-</style>
-
 <html>
 	<head>
 		<title>Hello World</title>
 	</head>
 	<body>
-		<h1>Hello World - Cool Feature 6</h1>
+		<h1>Hello World - Cool Feature PURLPLE</h1>
 		<a href="php-info.php" id="" title="php-info">php-info</a>
 	</body>
 </html>
