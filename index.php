da6ac8a01d2b60ed3471819d29cc1b106c46db2e
Greg Nokes <tsykoduk@gmail.com>
2018-01-31 16:41:57 -0800

    get the seconds var working correctly?

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 		<title>Hello World</title>
 		<?php 
 			$usec = ""; 
-			$usec = random_int ( 100000 , 2500000 );
+			$usec = random_int ( 10000 , 1000000 );
 			usleep ( $usec );
 			echo $usec;
 		?>
