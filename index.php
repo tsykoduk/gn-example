7fb93f7516bd9b4974073a071cc04b8472c62759
Greg Nokes <tsykoduk@gmail.com>
2018-12-05 08:41:03 -0800

    Update index.php

--- index.php
+++ index.php
@@ -13,15 +13,15 @@
 			switch ($multsec) {
    			case $multsec > 95:
 				$msec = 11;
-				error_log("multiplier : 11" , 0);
+				#error_log("multiplier : 11" , 0);
 				break;
 			case $multsec > 90 :  
     				$msec = 5;
-				error_log("multiplier : 5" , 0);
+				#error_log("multiplier : 5" , 0);
 				break;
 			case $multsec > 80 :
 				$msec = 2;
-				error_log("multiplier : 2" , 0);
+				#error_log("multiplier : 2" , 0);
 				break;
 			}
 			$rsec = ($usec + $tsec) * ($msec +1);
@@ -29,7 +29,7 @@
 			#for ($x = 0; $x <= 100; $x++) {
 			#	$leaker[] = $rsec;
 			#}
-			error_log("rsec pause length : " . $rsec, 0) #" leaker array count : " . count($leaker) , 0);
+			error_log("rsec pause length : " . $rsec . "Mult : " . $msec, 0) #" leaker array count : " . count($leaker) , 0);
 		?>
 	</head>
 	<body>
