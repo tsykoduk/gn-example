6b5215044b20e95d906b2e6518e38e133c3237ce
Greg Nokes <tsykoduk@gmail.com>
2020-01-10 09:07:50 -0800

    usleep is in millionths of a second. ugh.

--- index.php
+++ index.php
@@ -11,11 +11,11 @@
 			$msec = 0;
 			$multsec = "";
 			$leaker[] = "start";
-			$asec = mt_rand(0, 250);
-			$asec = mt_rand(0, 250);
-			$bsec = mt_rand(0, 250);
-			$csec = mt_rand(0, 250);
-			$dsec = mt_rand(250, 250);
+			$asec = mt_rand(0, 250000);
+			$asec = mt_rand(0, 250000);
+			$bsec = mt_rand(0, 250000);
+			$csec = mt_rand(0, 250000);
+			$dsec = mt_rand(250000, 250000);
 			$tsec = random_int ( 10 , 50000 );
 			$multsec = random_int ( 1, 1000 );
 			switch ($multsec) {
